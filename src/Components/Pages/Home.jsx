8c75823e6a7a7fc5df1a2661fd54c1e5705f629a
Saurav Paudel <Sauravpaudel22@gmail.com>
2024-04-28 11:20:23 +0545

    update

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -119,12 +119,38 @@ const Home = () => {
 
  {/* End of Card */}  
  {/* Testimonial Section */}
-        <div className='main bg-[#162545] w-full  rounded-2xl my-20'>
+       
+        <div data-hs-carousel='{
+    "loadingClasses": "opacity-0"
+  }' className="relative">
+  <div className="hs-carousel relative overflow-hidden w-full min-h-96 bg-white rounded-lg">
+    <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
+      <div className="hs-carousel-slide">
+        <div className="flex justify-center h-full bg-gray-100 p-6 dark:bg-neutral-900">
+          <span className="self-center text-4xl text-gray-800 transition duration-700 dark:text-white">First slide</span>
+        </div>
+      </div>
+      <div className="hs-carousel-slide">
+        <div className="flex justify-center h-full bg-gray-200 p-6 dark:bg-neutral-800">
+          <span className="self-center text-4xl text-gray-800 transition duration-700 dark:text-white">Second slide</span>
+        </div>
+      </div>
+      <div className="hs-carousel-slide">
+        <div className="flex justify-center h-full bg-gray-300 p-6 dark:bg-neutral-700">
+          <span className="self-center text-4xl text-gray-800 transition duration-700 dark:text-white">Third slide</span>
+        </div>
+      </div>
+    </div>
+  </div>
 
+  <div className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2">
+    <span className="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500"></span>
+    <span className="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500"></span>
+    <span className="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500"></span>
+  </div>
+</div>
         </div>
        
-
-  </div>
   )
 }
 
