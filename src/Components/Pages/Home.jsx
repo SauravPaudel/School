f790d6246757b2ba2a6dd7aaf1a6923ee16ea7fc
Saurav Paudel <Sauravpaudel22@gmail.com>
2024-05-06 14:15:19 +0545

    up

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import './Home.css';
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 import { FaQuoteLeft } from 'react-icons/fa';
