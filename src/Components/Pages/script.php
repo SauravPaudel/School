f3aca6dc1fe6171469e8de9ece518e6cb251b4d6
Saurav Paudel <Sauravpaudel22@gmail.com>
2024-05-07 15:26:27 +0545

    change

--- src/Components/Pages/script.php
+++ src/Components/Pages/script.php
@@ -21,7 +21,7 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 // Prepare and execute the SQL query
-$sql = "INSERT INTO contacts (fullname, phone, email, subject, message)
+$sql = "INSERT INTO test (fullname, phone, email, subject, message)
 VALUES ('$fullname', '$phone', '$email', '$subject', '$message')";
 
 if ($conn->query($sql) === TRUE) {
