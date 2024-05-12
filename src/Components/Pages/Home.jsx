d480bd562ed54987f1372f2d9cacd845c22ceca4
Saurav Paudel <Sauravpaudel22@gmail.com>
2024-05-12 13:40:30 +0545

    chnage

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -194,7 +194,7 @@ const Home = () => {
       <svg className="w-10 h-10 mb-2" fill="white" stroke="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
       </svg>
-      <span className="text-white text-center font-bold">10+ Years of Excellence</span>
+      <span className="text-white text-center font-bold">20+ Years of Excellence</span>
     </div>
     <div className="flex flex-col items-center hover:text-blue-500 transition duration-300">
       <svg className="w-10 h-10 mb-2" fill="white" stroke="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -206,13 +206,13 @@ const Home = () => {
       <svg className="w-10 h-10 mb-2" fill="white" stroke="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
       </svg>
-      <span className="text-white text-center">100+ Teaching Staffs</span>
+      <span className="text-white text-center">20+ Teaching Staffs</span>
     </div>
     <div className="flex flex-col items-center hover:text-blue-500 transition duration-300 mr-8">
       <svg className="w-10 h-10 mb-2" fill="white" stroke="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 7.944a11.955 11.955 0 01-8.618-2.03m0 0A12.027 12.027 0 007 12c0 2.556.686 4.94 1.898 6.975M7 12c0-2.59.911-4.984 2.417-6.865m0 0A12.027 12.027 0 0112 7.944a11.955 11.955 0 018.618 2.03m0 0A11.955 11.955 0 0112 21.956a11.955 11.955 0 01-8.618-2.03m9.605-4.93c.09-.184.088-.387.088-.593C12.972 14.174 11.8 13 10.516 13c-1.282 0-2.454 1.174-2.646 2.432-.091.206-.001.42.088.593z"></path>
       </svg>
-      <span className="text-white text-center">2+ Rewards</span>
+      <span className="text-white text-center">50+ Rewards</span>
     </div>
   </div>
 </div>
